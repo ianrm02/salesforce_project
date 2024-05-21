8ee2c9b9fe22292fc82e9c404fe4915dab19bb17
BRYCE MERRITT <merrittbryce@gmail.com>
2024-05-21 09:54:27 -0600

    started implementing basic filters

--- main/classifier.py
+++ main/classifier.py
@@ -1,24 +1,40 @@
-import common_alternates_db
 import input_standardization
 import load_standards
 import re
 import config
+from filter import *
 
 class Classifier:
     filters = []
     address_list = []
+    standard_df = None
 
     def __init__(self):
         self.address_list = input_standardization.generateStandardizedInput(config.DATASET_PATH, config.WORKING_DATASET)
         self.standard_df = load_standards.load_standards_from_config(config.STANDARDS_PATH)
 
+        #Filter System:
+        userCountry_f = userCountryFilter()
+        exactCountry_f = countryExactFilter()
 
+        self.filters = [
+            userCountry_f, exactCountry_f
+        ]
+
+    #TODO Might be obsolete, but leaving in just in case. Test for later.
     def _parseUserInput(userIn: str)->str:
         return re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", userIn.upper())
     
-
-    
-
-clf = Classifier()
-print(clf.address_list)
-
+    def applyFilterStack(self, rowInput:str):
+        """
+            Operational Order:
+            1. User Country Filter
+            2. Exact Country Filter
+            3. Fuzzy Country Filter
+            ----- UI Break: Country
+            4. User State Filter
+            5. Exact State Filter
+            6. Fuzzy State Filter.
+            ----- UI Break: State
+        """
+        pass
--- main/filter.py
+++ main/filter.py
@@ -1,33 +1,50 @@
 import common_alternates_db
+import re
 
 class Filter:
     ruleSet = []
     appliesTo = None #Enumerable: C S or A
 
-    def __init__(self):
-        self.ruleSet = None #expects a hashmap
+    def __init__(self, filterRule):
+        self.ruleSet = filterRule #expects a hashmap
         self.appliesTo = None
 
-    def __init__(self, filterRule:dict[str:str]):
-        self.ruleSet = filterRule
+    def _parseUserInput(self, userIn: str)->str:
+        return re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", userIn.upper())
+
 
     def applyFilter(rowInput: str):
-        return None
+        return (None, 0)
     
 
 class userCountryFilter(Filter):
-    def __init__(self):
+    def __init__(self, filterRule):
+        self.ruleSet = filterRule
         self.appliesTo = 'C'
+
+
     def applyFilter(self, rowInput: str):
-        if self.ruleSet.__contains__(rowInput):
+        if self.ruleSet.__contains__(self._parseUserInput(rowInput)):
             return (self.ruleSet[rowInput], 100)
         else:
             return (None, 0)
+        
+    def loadUserFilter(self, userCountryFilters):
+        pass
+        
+
+class countryExactFilter(Filter):
+    def __init__(self):
+        self.ruleSet = common_alternates_db.COMMON_ALTERNATES
+        self.appliesTo = 'C'
     
 
-
-filter = userCountryFilter()
-print(filter.applyFilter)
+    def applyFilter(self, rowInput: str):
+        parsedOutput = self._parseUserInput(rowInput)
+        if self.ruleSet.__contains__(parsedOutput):
+            return (self.ruleSet[parsedOutput], 100)
+        else:
+            return (None, 0)
     
 
     
