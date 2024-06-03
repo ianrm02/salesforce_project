e2c4382b85ccc451dc391c1e2b498d094ec8c1ba
Bryce Merritt <merrittbryce@gmail.com>
2024-06-03 17:13:57 -0600

    continued adding business logic to UI accordions

--- main/webapp.py
+++ main/webapp.py
@@ -10,10 +10,9 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = 'salesforcebutthesecondtime'
 clfApp = ClassifierApp()
 
-country_dropdown_ids = ['AF', 'AX', 'AL', 'DZ', 'AS', 'AD', 'AO', 'AI', 'AQ', 'AG', 'AR', 'AM', 'AW', 'AU', 'AT', 'AZ', 'BS', 'BH', 'BD', 'BB', 'BY', 'BE', 'BZ', 'BJ', 'BM', 'BT', 'BO', 'BQ', 'BA', 'BW', 'BV', 'BR', 'IO', 'BN', 'BG', 'BF', 'BI', 'CV', 'KH', 'CM', 'CA', 'KY', 'CF', 'TD', 'CL', 'CN', 'CX', 'CC', 'CO', 'KM', 'CG', 'CD', 'CK', 'CR', 'CI', 'HR', 'CU', 'CW', 'CY', 'CZ', 'DK', 'DJ', 'DM', 'DO', 'EC', 'EG', 'SV', 'GQ', 'ER', 'EE', 'SZ', 'ET', 'FK', 'FO', 'FJ', 'FI', 'FR', 'GF', 'PF', 'TF', 'GA', 'GM', 'GE', 'DE', 'GH', 'GI', 'GR', 'GL', 'GD', 'GP', 'GU', 'GT', 'GG', 'GN', 'GW', 'GY', 'HT', 'HM', 'VA', 'HN', 'HK', 'HU', 'IS', 'IN', 'ID', 'IR', 'IQ', 'IE', 'IM', 'IL', 'IT', 'JM', 'JP', 'JE', 'JO', 'KZ', 'KE', 'KI', 'KP', 'KR', 'KW', 'KG', 'LA', 'LV', 'LB', 'LS', 'LR', 'LY', 'LI', 'LT', 'LU', 'MO', 'MG', 'MW', 'MY', 'MV', 'ML', 'MT', 'MH', 'MQ', 'MR', 'MU', 'YT', 'MX', 'FM', 'MD', 'MC', 'MN', 'ME', 'MS', 'MA', 'MZ', 'MM', 'NA', 'NR', 'NP', 'NL', 'NC', 'NZ', 'NI', 'NE', 'NG', 'NU', 'NF', 'MK', 'MP', 'NO', 'OM', 'PK', 'PW', 'PS', 'PA', 'PG', 'PY', 'PE', 'PH', 'PN', 'PL', 'PT', 'PR', 'QA', 'RE', 'RO', 'RU', 'RW', 'BL', 'SH', 'KN', 'LC', 'MF', 'PM', 'VC', 'WS', 'SM', 'ST', 'SA', 'SN', 'RS', 'SC', 'SL', 'SG', 'SX', 'SK', 'SI', 'SB', 'SO', 'ZA', 'GS', 'SS', 'ES', 'LK', 'SD', 'SR', 'SJ', 'SE', 'CH', 'SY', 'TW', 'TJ', 'TZ', 'TH', 'TL', 'TG', 'TK', 'TO', 'TT', 'TN', 'TR', 'TM', 'TC', 'TV', 'UG', 'UA', 'AE', 'GB', 'US', 'UM', 'UY', 'UZ', 'VU', 'VE', 'VN', 'VG', 'VI', 'WF', 'EH', 'YE', 'ZM', 'ZW']
-state_dropdown_ids = {'AR':['ER'], 'US':['CO', 'DE', 'TX']}
-affected_ccodes = ['AR', 'US']
-affected_scodes = {'AR':['ER'], 'US':['CO']}
+country_dropdown_ids = clfApp.clf.iso_standard_df["alpha-2"]
+state_dropdown_ids = {'AR':['ER'], 'US':['CO', 'DE', 'TX']} #TODO
+affected_scodes = {'AR':['ER'], 'US':['CO']} #TODO
 address_batch = [['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '12 ARROZ CT', 80], ['US', 'CO', '4 Littleton Dr', 12], ['US', 'CO', '400 Colo St.', 20], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', '123 Arentin Ln.', 50], ['AR', 'ER', 'DO NOT DISPLAY', 50]]
 # LIST MUST BE SORTED BY CONFIDENCE BEFORE PASSED TO PAGE
 
@@ -37,41 +36,62 @@ def home():
 
     return render_template('home.html')
 
+
 @app.route("/country_approve") # define path component
 def country_approve():
     #TODO loading screen to show it's in processing
-    country_changes = [item for item in clfApp.db_handler.get_all_from_table("CountryChanges")].sort(key=lambda x: x[3])
+    country_changes = [item for item in clfApp.db_handler.get_all_from_table("CountryChanges")]
+    country_changes.sort(key=lambda x: x[3])
     #Sorted by confidence
+    #Each address has [OldCo] [NewCo] [Freq] [Conf]
 
-    #TODO logic to get from changes to the format needed bby the render_template()
+    print(country_changes)
 
-    return render_template('country_skeleton.html', aff_country_codes = affected_ccodes, dropdown_ids = country_dropdown_ids, change_ids = change_ids)
+    affected_ccodes = list(set([code[1] for code in country_changes]))
+    affected_ccodes.sort()
+
+    country_change_ids = [['C'] + code for code in country_changes]
+
+    return render_template('country_skeleton.html', aff_country_codes = affected_ccodes, dropdown_ids = country_dropdown_ids, change_ids = country_change_ids)
+
 
 @app.route("/state_approve")
 def state_approve():
-    state_changes = [item for item in clfApp.db_handler.get_all_from_table("StateChanges")].sort(key=lambda x: x[3])
+    state_changes = [item for item in clfApp.db_handler.get_all_from_table("StateChanges")]
+    state_changes.sort(key=lambda x: x[3])
     #Sorted by confidence
+    #Each address has [Old] [New] [Freq] [Conf]
 
     #TODO logic to get from changes to the format needed bby the render_template()
 
+    print(state_changes)
+
     return render_template('state_skeleton.html', aff_country_codes = affected_ccodes, aff_state_codes = affected_scodes, cdropdown_ids = country_dropdown_ids, sdropdown_ids = state_dropdown_ids, cstate_ids = state_dropdown_ids, change_ids = change_ids)
+
 
 @app.route("/address_approve")
 def address_approve():
-    address_changes = [item for item in clfApp.db_handler.get_all_from_table("AddressChanges")].sort(key=lambda x: x[3])
+    address_changes = [item for item in clfApp.db_handler.get_all_from_table("AddressChanges")]
+    address_changes.sort(key=lambda x: x[3])
     #Sorted by confidence
 
     #TODO logic to get from changes to the format needed bby the render_template()
 
     return render_template('address_skeleton.html', aff_country_codes = affected_ccodes, aff_state_codes = affected_scodes, cdropdown_ids = country_dropdown_ids, sdropdown_ids = state_dropdown_ids, cstate_ids = state_dropdown_ids, address_info = address_batch)
 
+
 @app.route("/statistics")
 def statistics():
     return render_template('statistics.html')
 
+
 @app.route("/end")
 def end():
     return render_template('endscreen.html')
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
