841a236fb6586436df092343432afcc4adc13bf3
Calvin Ko <calkwko@gmail.com>
2024-06-05 15:39:27 -0600

    Block user until all addresses are approved for country page

--- main/static/javascript/approval.js
+++ main/static/javascript/approval.js
@@ -1,5 +1,4 @@
-
-var sdropdown_ids = {"US":["TX", "DE"], "AR":["ER"]};
+var sdropdown_ids = { "US": ["TX", "DE"], "AR": ["ER"] };
 var jQueryScript = document.createElement('script');
 jQueryScript.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/');
 document.head.appendChild(jQueryScript);
@@ -7,17 +6,17 @@ activateAccordions();
 checkApproved();
 
 // Allows accordions to actually extend and function
-function activateAccordions(){
+function activateAccordions() {
   var acc = document.getElementsByClassName("accordion");
   for (var i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
     });
   }
 }
@@ -25,27 +24,27 @@ function activateAccordions(){
 // Takes input for the next page and makes the url. We don't have pages sorted into folders so using the root(/) allow us to make the url work
 // More complex webapp would need to utilize url_for or something similar
 function redirect(next_page) {
-  window.location.href = "/"+next_page;
+  window.location.href = "/" + next_page;
 }
 // Repopulates state dropdowns to match country selection
-function repopulate_statedropdown(ccode, fieldstring){
+function repopulate_statedropdown(ccode, fieldstring) {
   // Find matching dropdown using accordion country code and the associated string in the row
   var cdropdown = document.getElementById("cdropdown" + ccode + fieldstring);
   var nccode = cdropdown.options[cdropdown.selectedIndex].text; // The newly selected code
   var sdropdown = document.getElementById("sdropdown" + ccode + fieldstring);
   // Clear dropdown
-  while(sdropdown.options.length > 0){
+  while (sdropdown.options.length > 0) {
     sdropdown.remove(0);
   }
   // Mark as empty if there are no associated states
-  if(!(nccode in sdropdown_ids)){
+  if (!(nccode in sdropdown_ids)) {
     var emptyoption = document.createElement("option");
     emptyoption.text = "No states available";
     sdropdown.appendChild(emptyoption);
     return;
   }
   // Repopulate with new options matching new states
-  for(var i = 0; i < sdropdown_ids[nccode].length; i++){
+  for (var i = 0; i < sdropdown_ids[nccode].length; i++) {
     var newOption = document.createElement("option");
     newOption.text = sdropdown_ids[nccode][i];
     sdropdown.appendChild(newOption);
@@ -54,7 +53,7 @@ function repopulate_statedropdown(ccode, fieldstring){
 
 // Allows for "Approve All" feature called by upper checkbox
 // Should only apply to all in the accordion(matching country code in the tag)
-function selectAll(ccode){
+function selectAll(ccode) {
   //Auto select feature
   var newCheckVal = document.getElementById("approveall" + ccode).checked;
   var checks = document.getElementsByName("check" + ccode);
@@ -64,7 +63,7 @@ function selectAll(ccode){
   }
 }
 
-function checkApproved(){
+function checkApproved() {
   var tables = document.getElementsByTagName("table");
 
   for (var i = 0; i < tables.length; i++) {
@@ -76,6 +75,26 @@ function checkApproved(){
         approveAllButton.checked = false;
       } else {
         approveAllButton.checked = true;
+      }
+    }
+  }
+}
+
+function enableButton() {
+  var allCheckedList = document.getElementsByClassName("approveall");
+  var nextButton = document.getElementById("nextPage");
+  var numChecked = 0;
+
+  for (var i = 0; i < allCheckedList.length; i++) {
+    if (allCheckedList[i].checked) {
+      numChecked++;
+      if (numChecked == allCheckedList.length) {
+        nextButton.disabled = false;
+      }
+    } else {
+      numChecked--;
+      if (numChecked != allCheckedList.length) {
+        nextButton.disabled = true;
       }
     }
   }
